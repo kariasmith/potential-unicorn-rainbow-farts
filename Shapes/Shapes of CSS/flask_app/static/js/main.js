ca85db0e4741065d52a3b1aa97f0513430a5e75d
Kari Smith <kariasmith@hotmail.com>
2023-11-07 00:27:33 -0700

    Update main.js

--- Shapes/Shapes of CSS/flask_app/static/js/main.js
+++ Shapes/Shapes of CSS/flask_app/static/js/main.js
@@ -1,1 +1,16 @@
+console.log("Running main.js")
+
+function logout(element) {
+    element.innerText = "Logout";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function increase(element) {
+    element.innerText = parseInt(element.innerText) + 1;
+    
+}
+
 
