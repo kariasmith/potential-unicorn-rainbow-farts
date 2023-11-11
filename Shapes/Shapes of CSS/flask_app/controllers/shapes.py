0a85bf4af5dfc94f00f566dd335bbac1c2dad14c
Kari Smith <kariasmith@hotmail.com>
2023-11-10 19:50:36 -0700

    Update shapes.py

--- Shapes/Shapes of CSS/flask_app/controllers/shapes.py
+++ Shapes/Shapes of CSS/flask_app/controllers/shapes.py
@@ -1,6 +1,10 @@
 from flask_app import app
-from flask import render_template
+from flask import render_template, redirect, request, session, flash
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/myshapes')
+def myshapes():
+    return render_template("myshapes.html")
